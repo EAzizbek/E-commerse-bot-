ae538762e73bb78244b4d6e32fcf7db7d44f46fa
EAzizbek <a.ergashov1308@gmail.com>
2026-02-06 19:38:14 +0500

    first commit

---
+++ handlers/register.py
@@ -0,0 +1,40 @@
+from aiogram import Router,F
+from aiogram.types import Message
+from states.register import RegisterState
+from aiogram.fsm.context import FSMContext
+
+
+router=Router()
+
+@router.message(F.text=="Register")
+async def register_handler(msg:Message,state:FSMContext):
+    await msg.answer("Registratsiyadan o'tish uchun ismingizni kiriting: ")
+    await state.set_state(RegisterState.name)
+
+@router.message(RegisterState.name)
+async def register_handler(msg:Message,state:FSMContext):
+    await state.update_data(name=msg.text)
+    await msg.answer("Familyangizni kiriting: ")
+    await state.set_state(RegisterState.surename)
+
+@router.message(RegisterState.surename)
+async def register_handler(msg:Message,state:FSMContext):
+    await state.update_data(surename=msg.text)
+    await msg.answer("Yoshingizni kiriting: ")
+    await state.set_state(RegisterState.age)
+
+@router.message(RegisterState.age)
+async def register_handler(msg:Message,state:FSMContext):
+    await state.update_data(age=msg.text)
+    await msg.answer("Raqamingizni  kiriting: ")
+    await state.set_state(RegisterState.number)
+
+@router.message(RegisterState.number)
+async def register_handler(msg:Message,state:FSMContext,db):
+    await state.update_data(number=msg.text)
+    
+    data=await state.get_data()
+    await msg.answer(text=f'Ismingiz: {data['name']}\nFamilyangiz: {data['surename']}\nYoshingiz: {data['age']}\nRaqamingiz: {data['number']}\n')
+    await db.add_user(msg.from_user.id,data["name"],data["surename"],data["age"],data["number"])
+
+    await state.clear()
---
+++ handlers/start.py
@@ -0,0 +1,10 @@
+from aiogram.filters import CommandStart
+from aiogram import Router
+from aiogram.types import Message
+from keyboards.reply import start_reply,register_reply
+
+router=Router()
+
+@router.message(CommandStart())
+async def start_handler(msg:Message):
+    await msg.answer(f"Assalomu Alaykum {msg.from_user.full_name}, botga xush kelibsiz",reply_markup=register_reply())
